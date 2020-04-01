61bfef292e80031c90299bf8c669b73d3b36aed9
Pavel-JavaIntro <62737771+Pavel-JavaIntro@users.noreply.github.com>
2020-04-01 07:48:00 +0300

    Update BasicHelper.java

--- src/by/pavka/basic/util/BasicHelper.java
+++ src/by/pavka/basic/util/BasicHelper.java
@@ -19,19 +19,5 @@ public class BasicHelper {
 		return number;
 	}
 
-	public static void printArray(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i] + " ");
-
-		}
-		System.out.println();
-	}
-
-	public static void printArray(double[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i] + " ");
-
-		}
-		System.out.println();
-	}
+	
 }
